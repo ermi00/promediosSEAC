d07140808b5f4999e48b32b8a26eca165e615aac
ermi00 <miguelgonzalez.280803@gmail.com>
2025-09-17 22:56:36 -0600

    Impresion de promedio

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2");
 let input3 = document.getElementById("input3");
 let botonCalcular = document.getElementById("botonCalcular");
+let divResultados = document.getElementById("divResultados");
 let promedioGeneral;
+let promedioMateria;
+
+divResultados.style.display = "none";
 
 function mostrarCalificaciones() {
   let cf1 = Number(input1.value);
@@ -10,8 +14,9 @@ function mostrarCalificaciones() {
   let cf3 = Number(input3.value);
   promedioGeneral = ObtenerPromedioGeneral(cf1, cf2, cf3);
 
-  console.log("Tu promedio general es " + promedioGeneral.toFixed(1))
+  divResultados.style.display = "flex"
   pasarCuatrimestre(8.5);
+  cambiarResultados();
 }
 
 function ObtenerPromedioGeneral(a, b, c) {
@@ -24,8 +29,8 @@ function ObtenerPromedioFinal(promedioGeneral, examenFinal) {
 
 function pasarCuatrimestre(promedioExentar) {
   if (promedioGeneral > promedioExentar) {
-    console.log("Pasaste el cuatrimestre");
+    divResultados.innerHTML = `<h1>${promedioGeneral.toFixed(1)}</h1><p>¡Aprobaste!</p>`;
   } else {
-    console.log("Tendras que hacer examen final");
+    divResultados.innerHTML = `<h1>${promedioGeneral.toFixed(1)}</h1><p>Tendras que hacer examen final</p>`;
   }
 }
