21f88d05e566b4a9adee4700206f37b916f3b1ef
ermi00 <miguelgonzalez.280803@gmail.com>
2025-09-20 21:18:54 -0600

    Gifs, mejoras de funcionamiento, nuevas funciones

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ let divResultados = document.getElementById("divResultados");
 let divCalificaciones = document.getElementById("divCalificaciones");
 let divPregunta = document.getElementById("divPregunta");
 let pInstruccion = document.getElementById("pInstruccion");
+let titulo = document.getElementById("tituloH1");
 let promedioGeneral;
 let promedioMateria;
 let promedioFinal;
 let logo = document.getElementById("logo");
+let sadSong = new Audio("./assets/sadSong.mp3");
+let goodSong = new Audio("./assets/goodSong.mp3");
 
 divCalificaciones.style.display = "none";
 divResultados.style.display = "none";
@@ -52,13 +55,60 @@ function obtenerPromedioFinal(promedioGeneral, examenFinal) {
 
 function pasarCuatrimestre(promedioExentar) {
   if (promedioGeneral >= promedioExentar) {
-    divResultados.innerHTML = `<h1>${promedioGeneral.toFixed(
-      1
-    )}</h1><p>¡Aprobaste!</p>`;
+    goodSong.play();
+    divResultados.innerHTML = `
+        <div class="divResultados" id="divResultados">
+        <h1 style="color: #00ff15;">APROBASTE</h1>
+        <img id="imgGIF" src="./assets/personajeLoop.gif" alt="">
+        <h1>${promedioGeneral.toFixed(1)}</h1>
+        <p>La materia se pasa con ${promedioMateria}, por lo que podras dormir tranquilo esta noche.</p>
+        <button><a href="./index.html">IR AL INICIO</a></button>
+    </div>
+    `;
+    let imgGIF = document.getElementById("imgGIF");
+    titulo.style.display = "none";
+    // setTimeout(() => {imgGIF.src = "./assets/personajeBaile.gif"}, 8500)
   } else {
-    divResultados.innerHTML = `<h1>${promedioGeneral.toFixed(
-      1
-    )}</h1><p>La materia se excenta con <strong>${promedioMateria}</strong>, por lo que tendras que hacer examen final.</p>`;
+    divResultados.innerHTML = `
+    <div class="divResultados" id="divResultados">
+        <h1 style="font-family: 'Sixtyfour Convergence';">REPROBASTE</h1>
+        <img src="./assets/personajeTriste.png" alt="">
+        <h1>${promedioGeneral.toFixed(1)}</h1>
+        <p>La materia se pasa con ${promedioMateria}, tendras que hacer examen final.</p>
+        <button id="btn1">IR AL INICIO</button>
+    </div>
+    `;
+    titulo.style.display = "none";
+    sadSong.play();
+    let btn1 = document.getElementById("btn1");
+    let frases = [
+      "",
+      "QUIERES IR AL INICIO?",
+      "ENSERIO?",
+      "NOLOSE AMIGO",
+      "VE A ESTUDIAR MEJOR",
+      "LO DIGO ENSERIO, ME PREOCUPO POR TI",
+      "ES DECIR...",
+      "SE QUE NO ES FACIL PARA NINGUNO",
+      "REALMENTE PARECIA QUE TE ESFORZABAS",
+      "PERO HEY...",
+      "TODOS MERECEMOS SEGUNDAS OPORTUNIDADES",
+      "ASI QUE, INICIEMOS DE NUEVO",
+      "PERO ESTA VEZ, EN SERIO, ESTUDIA",
+      "BUENA SUERTE",
+    ];
+    let contador = 0;
+
+    btn1.addEventListener("click", () => {
+      contador++;
+      btn1.innerHTML = frases[contador];
+      if (contador > 12) {
+        btn1.innerHTML =
+          "IR AL INICIO";
+          window.location.replace("./index.html");
+          sadSong.pause();
+      }
+    });
   }
 }
 
